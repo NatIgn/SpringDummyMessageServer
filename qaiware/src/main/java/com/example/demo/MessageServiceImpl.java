019d2a487bf2416a01912e7b7a1be35beb6dad61
unknown <nsignatovan@gmail.com>
2021-11-08 20:27:53 +0200

    Added some changes

--- qaiware/src/main/java/com/example/demo/MessageServiceImpl.java
+++ qaiware/src/main/java/com/example/demo/MessageServiceImpl.java
@@ -7,13 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class MessageServiceImpl implements MessageService {
 
-//	private MessageDAO message;
 	private MessageRepository messageRepo;
-	
-//	@Autowired
-//	public MessageServiceImpl(MessageDAO theMessage) {
-//		message = theMessage;
-//	}
 	
 	@Autowired
 	public MessageServiceImpl(MessageRepository theMessageRepo) {
@@ -24,7 +18,6 @@ public class MessageServiceImpl implements MessageService {
 	@Transactional
 	public void save(Message theMessage) {
 		messageRepo.save(theMessage);
-
 	}
 
 }
